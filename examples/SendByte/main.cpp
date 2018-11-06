fb143b09e8729ca4f50f8c7ae553617435565b50
Brandon Kroes <b.kroes@protonmail.com>
2018-11-06 11:31:40 +0100

    added dummy trigger class

--- examples/SendByte/main.cpp
+++ examples/SendByte/main.cpp
@@ -2,6 +2,31 @@
 #include "IrTransmitter.hpp"
 #include "SendIrMessageControl.hpp"
 
+class DummySendIrMessageControl: public rtos::task<>{
+
+  private:
+
+    SendIrMessageControl & sirmc;
+
+
+
+  public:
+
+    DummySendIrMessageControl(SendIrMessageControl & sirmc):sirmc(sirmc){}
+
+    void main ()override{
+      while(true){
+        hwlib::wait_ms(2000);
+        hwlib::cout << "send to ir";
+        sirmc.sendMessage(170); // blink interval
+		    hwlib::wait_ms(20000);
+      }
+    }
+
+};
+
+
+
 int main(){
   WDT->WDT_MR = WDT_MR_WDDIS;
 	hwlib::wait_ms( 1000 );
@@ -11,15 +36,8 @@ int main(){
 	IrTransmitter ir(OUT);
 
   SendIrMessageControl sir(ir, wait_time*20, wait_time*20, 1);
-  //example sos:
-  // 1 2 3 4 5 6 9 10 13 14 17 18 19 20 21
-  // ·   ·   ·   —    —     —     ·  ·  ·
-  uint16_t oneb = 255;
 
+  DummySendIrMessageControl dummytrigger(sir);
+  rtos::run();
 
-
-	for(;;){
-    sir.sendMessage(oneb);
-  	hwlib::wait_ms( 1000 );
-	}
 }
