945c39b6c0d75a1aaac3f41a073441c79c88effa
Brandon Kroes <b.kroes@protonmail.com>
2018-11-07 17:27:14 +0100

    Added Doxygen

--- lib/IrTransmitter.hpp
+++ lib/IrTransmitter.hpp
@@ -11,7 +11,13 @@ private:
     hwlib::target::d2_36kHz & IrLED;
 
 public:
+
+    //! Initialize a SendIrMessageControl object
+    //! @param IrLED - 36khz IRLed needs to be used for PWM capbilities
     IrTransmitter(hwlib::target::d2_36kHz & IrLED):IrLED(IrLED){};
+
+
+    //! @param b a boolean which is the new value of the IrLED until changed
     void Send(bool b){
           IrLED.set(b);
     };
--- lib/SendIrMessageControl.hpp
+++ lib/SendIrMessageControl.hpp
@@ -17,12 +17,26 @@ private:
     rtos::channel< int, 16 > IRMessagesSendQueue;
 
 public:
+    //! SendIrMessageControl waiting for a hit to the queue
+    //! @param IrTransmitter led that will be toggled
+    //! @param low decides the positive low and negative low
+    //! @param High decides the positive High and negative High
+    //! @param priority gives the task it's priority
     SendIrMessageControl(IrTransmitter & IrLED,
                   uint16_t low,
                   uint16_t high,
                   int priority):
     task(priority, "SendIrMessageControl"), IrLED(IrLED), negHigh(low), negLow(high), posHigh(high), posLow(low), SendTimer( this, "SendTimer" ), IRMessagesSendQueue( this, "IRMessagesSendQueue" ){};
 
+
+
+    //! SendIrMessageControl waiting for a hit to the queue
+    //! @param IrTransmitter led that will be toggled
+    //! @param negLow decides the negative low
+    //! @param negHigh decides the negative High
+    //! @param posLow decides the positive low
+    //! @param posHigh decides the positive High
+    //! @param priority gives the task it's priority
     SendIrMessageControl(IrTransmitter & IrLED,
                   uint16_t negLow,
                   uint16_t negHigh,
@@ -32,9 +46,13 @@ public:
     task(priority, "SendIrMessageControl"),IrLED(IrLED), negHigh(negHigh), negLow(negLow), posHigh(posHigh), posLow(posLow),SendTimer( this, "SendTimer" ), IRMessagesSendQueue( this, "IRMessagesSendQueue" ){};
 
 
+
+    //! @param m is the coded message uint16_t
     void sendMessage(uint16_t m){
         IRMessagesSendQueue.write(m);
     }
+
+
     void main(){
       enum class STATE {WAIT_MESSAGE, WAIT_SECOND_MESSAGE, SENDING_BIT};
       wait(IRMessagesSendQueue);
@@ -59,8 +77,8 @@ public:
               SendTimer.set(3000);
               wait(SendTimer);
               S = STATE::SENDING_BIT;
-			  
-			  
+
+
             case STATE::SENDING_BIT:
               bit = (dataIn>>i)& 1;
               if(bit == 1){
@@ -72,10 +90,10 @@ public:
               }
               IrLED.Send(true);
               SendTimer.set(w1);
-			  wait(SendTimer);
+			           wait(SendTimer);
               IrLED.Send(false);
               SendTimer.set(w2);
-			  wait(SendTimer);
+			           wait(SendTimer);
               if(i == 0){
                   n++;
                   if(n==2){
